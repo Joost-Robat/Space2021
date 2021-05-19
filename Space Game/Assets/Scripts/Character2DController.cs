e6ba6a48f46327d3723c9fb2d613ec692b206de9
SennoTaphoorn <29040@ma-web.nl>
2021-05-19 11:57:44 +0200

    begin

---
+++ Space Game/Assets/Scripts/Character2DController.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+
+public class Character2DController : MonoBehaviour
+{
+    public float MovementSpeed = 1;
+    public float JumpForce = 1;
+
+    private Rigidbody2D _rigidbody;
+    private bool facingRight = true;
+
+    private void Start()
+    {
+        _rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        var movement = Input.GetAxis("Horizontal");
+        transform.position += new Vector3(movement, 0, 0) * Time.deltaTime * MovementSpeed;
+
+        if (movement > 0 && !facingRight)
+        {
+            FlipCharacter();
+        }
+        else if (movement < 0 && facingRight)
+        {
+            FlipCharacter();
+        }
+
+        if (Input.GetButtonDown("Jump") && Mathf.Abs(_rigidbody.velocity.y) < 0.001f)
+        {
+            _rigidbody.AddForce(new Vector2(0, JumpForce), ForceMode2D.Impulse);    
+        }
+    }
+    private void FlipCharacter()
+    {
+        facingRight = !facingRight;
+        transform.Rotate(0f, 180f, 0f);
+    }
+}
