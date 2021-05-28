255da52ab565f2ec8f5d3bb235298334ddf8047c
Joost-Robat <30769@ma-web.nl>
2021-05-28 14:36:12 +0200

    Verder gegaan met Enemy AI script, beetje met ranges gekloot en het werkt nu, ik ga zo jump feature oprecht toevoegen maar moest mijn idee eerst perfectioneren.

--- Space Game/Assets/Scripts/EnemyBehaviour.cs
+++ Space Game/Assets/Scripts/EnemyBehaviour.cs
@@ -9,10 +9,11 @@ public class EnemyBehaviour : MonoBehaviour
     GameObject player;
     private bool facingRight = true;
     public bool isOnPlatform = false;
-    private float rangePlayerY;
+    private float PlayerYTest;
     private float rangePlayerX;
     private float xMinus;
     private float xPlus;
+    public float jumpTrue;
     // Start is called before the first frame update
     void Start()
     {
@@ -25,14 +26,24 @@ public class EnemyBehaviour : MonoBehaviour
         GameObject player = GameObject.Find("Player");
         playerY = player.transform.position.y;
         playerX = player.transform.position.x;
-        xMinus = playerX - 10;
-        xPlus = playerX + 10;
+        PlayerYTest = player.transform.position.y - 1;
+        xMinus = playerX - 30;
+        xPlus = playerX + 30;
         if(transform.position.x >= playerX)
             // Plus if statement
         {
             if(xPlus <= transform.position.x)
             {
-
+                if(transform.position.y <= PlayerYTest)
+                {
+                    jumpTrue += 10 * Time.deltaTime;
+                    Debug.Log(jumpTrue);
+                }
+                else
+                {
+                    jumpTrue = 0;
+                    Debug.Log(jumpTrue);
+                }
             }
         }
         if(transform.position.x <= playerX)
@@ -40,7 +51,16 @@ public class EnemyBehaviour : MonoBehaviour
         {
             if(xMinus <= transform.position.x)
             {
-
+                if(transform.position.y <= PlayerYTest)
+                {
+                    jumpTrue += 10 * Time.deltaTime;
+                    Debug.Log(jumpTrue);
+                }
+                else
+                {
+                    jumpTrue = 0;
+                    Debug.Log(jumpTrue);
+                }
             }
         }
         if(gameObject.transform.position.y <= playerY)
@@ -61,7 +81,6 @@ public class EnemyBehaviour : MonoBehaviour
             transform.position -= new Vector3(2f, 0f, 0f) * Time.deltaTime;
             flip(true);
         }
-        rangePlayerY = playerY;
     }
     void flip(bool Statement)
     {
